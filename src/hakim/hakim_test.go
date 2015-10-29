cb10c2a86e03c6cafecbae86538a588bd24e2917
Anthony Emengo <aemengo@pivotal.io>
2015-10-29 12:17:16 -0400

    Initial commit
    Signed-off-by: Ben Moss <bmoss@pivotal.io>

---
+++ src/hakim/hakim_test.go
@@ -0,0 +1,12 @@
+package main_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Hakim", func() {
+	It("should be working", func() {
+		Expect(2).To(Equal(2))
+	})
+})
